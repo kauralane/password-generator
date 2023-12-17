d44e7ba27a9123873ab0359d96c20654d4ce311a
Laura Kane <148717413+kauralane@users.noreply.github.com>
2023-12-17 18:40:21 +0000

    first additions to README file.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# password-generator
-Application to randomly generate a password for the user
+# Password Generator
+This is an in-browser application that makes use of JavaScript to randomly generate a password, based on the criteria that the user sets. 
+
+The user is able to determine the length of password and which types of characters are included, from a series of prompts/confirms. 
+
+The user then presses the 'generate password' button and their password will appear in the box. 
+
+[Deployed application]()
+
+[Screenshot]()
+
+## Installation
+None.
+
+## Usage
+Give a numerical answer for how long you would like your password. Then press 'confirm' (yes) or 'cancel' (no) to indicate whether you want uppercase, lowercase, special, and/or numerical characters. Then press the 'generate password' button to create your password.
+
+## Credits
+W3 schools website
+mdn web docs website
+
+## License
+Licensed under the MIT License.
--- script.js
+++ script.js
@@ -1,47 +1,16 @@
-// PSEUDOCODE
-
-// Generate a password when the button is clicked
-// Button --> onclick --> call function that generates password
-
-// Present a series of prompts for password criteria
-// Prompt(Select how many characters you would like in your password, between 8 and 128 inclusive.) - will need ParseInt
-// Confirm(Would you like lowercase?)
-// Confirm(Would you like uppercase?)
-// Confirm(Would you like numeric characters?)
-// Confirm(Would you like special characters?)
-
-// need to store each answer in a variable
-// number in prompt 1 needs to be length of password
-// MAYBE - if confirm = true, add that array to a new array. Then, you will have one array containing all the different characters that can be used. Select characters at random from this new array, based on the 'length' input. (But how to ensure that at least one type of each character is taken? Something to do with index numbers?) 
-
-// Perhaps - nested array. Then you can select to choose x characters from each array subset?
-
-// Length of password
-// At least 8 characters but no more than 128.
-// Character types
-// Lowercase
-// Uppercase
-// Numeric
-// Special characters($@%&*, etc)
-
-// Code should validate for each input and at least one character type should be selected
-
-// Once prompts are answered then the password should be generated and displayed in an alert or written to the page
-// alert(Your password is: [var1][var2][var3][var4])
-
 // Array of special characters to be included in password - 23
-var specialCharacters = ['@', '%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
 // Array of numeric characters to be included in password - 10
 var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 // Array of lowercase characters to be included in password - 26
-var lowercaseCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // Array of uppercase characters to be included in password - 26
-var uppercaseCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-// Function to prompt user for password options
+// Function to prompt user for password length
 function getPasswordLength() {
   lengthString = prompt(`How long would you like your password to be? Please choose between 8 and 128 characters, inclusive.`)
   lengthNum = parseInt(lengthString);
@@ -49,10 +18,11 @@ function getPasswordLength() {
   if (lengthString < 8 || lengthString > 128) {
     alert(`You must select a length betwen 8 and 128, inclusive. Please try again.`);
     getPasswordLength()
-    // each time you do this wrong, when you then do it right, it asks you the prompts as many times as you got this wrong
-  }}
+  }
+}
 
-  function getPasswordOptions() {
+// Function to prompt user for character options
+function getPasswordOptions() {
   lowercaseConfirm = confirm(`Do you want lowercase characters?`);
 
   uppercaseConfirm = confirm(`Do you want uppercase characters?`);
@@ -66,8 +36,8 @@ function getPasswordLength() {
     getPasswordOptions()
   }
 
-  return lengthNum, lowercaseConfirm, uppercaseConfirm, numericConfirm 
-  }
+  return lengthNum, lowercaseConfirm, uppercaseConfirm, numericConfirm
+}
 
 getPasswordLength()
 getPasswordOptions()
@@ -83,8 +53,8 @@ const passwordChars = [];
 // Process for generating random character for each of the sets selected by the user. One character of each type is pushed into the 'passwordChars' array. The remaining number of characters is chosen from the 'characterMix' array, which is all the selected character set arrays, joined into one.
 if (lowercaseConfirm === true) {
   randomLower = (getRandom(lowercaseCharacters));
-passwordChars.push(randomLower)
-characterMix.push(lowercaseCharacters)
+  passwordChars.push(randomLower)
+  characterMix.push(lowercaseCharacters)
 } if (uppercaseConfirm === true) {
   randomUpper = (getRandom(uppercaseCharacters));
   passwordChars.push(randomUpper)
@@ -106,8 +76,9 @@ let characterArray = characterMix.flat();
 function generatePassword() {
   for (let i = passwordChars.length; i < lengthNum; i++) {
     partTwo = (getRandom(characterArray));
-    passwordChars.push(partTwo)}
-    return passwordChars.join("")
+    passwordChars.push(partTwo)
+  }
+  return passwordChars.join("")
 }
 
 // Get references to the #generate element
